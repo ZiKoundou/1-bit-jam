98d7f44ee51ed93077b2b893a5b9b36713d91bc9
Zion Koundou <114265782+ZiKoundou@users.noreply.github.com>
2026-02-26 14:38:59 -0800

    adding a camera offset and updating scene artwork

---
+++ ClockOut/Assets/Scripts/CameraScripts/CameraMouseOffset.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CameraMouseOffset : MonoBehaviour
+{
+    public Transform player;
+    public float maxOffset = 2f;
+    public float smoothSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector2 mouseScreen = Mouse.current.position.ReadValue();
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
+        mouseWorld.z = 0f;
+
+        Vector3 direction = mouseWorld - player.position;
+        Vector3 offset = Vector3.ClampMagnitude(direction, maxOffset);
+
+        Vector3 targetPos = player.position + offset;
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            targetPos,
+            Time.deltaTime * smoothSpeed
+        );
+    }
+}
